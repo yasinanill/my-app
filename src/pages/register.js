ca4c458d26bf0d3623f89364590b28d55a212c29
yasin anil <yasinkaya687@gmail.com>
2022-06-30 16:25:30 +0300

    master

---
+++ src/pages/register.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+
+import { register } from "../firebase";
+
+
+
+
+const Register = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await register(email, password);
+
+  };
+
+  return (
+    <div className="App">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="nome@email.com.br"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="primary">KAYDOL</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
